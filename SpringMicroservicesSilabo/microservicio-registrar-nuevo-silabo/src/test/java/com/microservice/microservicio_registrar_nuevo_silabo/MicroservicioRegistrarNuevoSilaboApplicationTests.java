19aa6d3c073f12dcdb6c48590e600641a6cabc09
plzkllm <axl_parks@yahoo.com.pe>
2025-04-23 11:26:41 -0500

    Configuración del proyecto hijo con el proyecto padre

---
+++ SpringMicroservicesSilabo/microservicio-registrar-nuevo-silabo/src/test/java/com/microservice/microservicio_registrar_nuevo_silabo/MicroservicioRegistrarNuevoSilaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.microservicio_registrar_nuevo_silabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioRegistrarNuevoSilaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
